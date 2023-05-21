b6140a1397a023aa6b400c0766f4ed94512fd524
Aftab7620 <mohdaftab7620@gmail.com>
2023-05-21 23:28:15 +0530

    Create FuncOver.java

---
+++ FuncOver.java
@@ -0,0 +1,29 @@
+class Parent
+{
+	void show()
+	{
+		System.out.println("Parent's show()");
+	}
+}
+
+// Inherited class
+class Child extends Parent
+{
+	// This method overrides show() of Parent
+	void show()
+	{
+		System.out.println("Child's show()");
+	}
+}
+
+// Driver class
+class FuncOver
+{
+	public static void main(String args[])
+	{
+		Parent obj1 = new Parent();
+		obj1.show();
+		Parent obj2 = new Child();
+		obj2.show();
+	}
+}
