68466ca68411fb3f06b1facf0fd34f646b91836e
Aftab7620 <mohdaftab7620@gmail.com>
2023-05-27 23:37:26 +0530

    Create Constex.java

---
+++ Constex.java
@@ -0,0 +1,26 @@
+public class Constex {  
+        int id;  
+        String name;  
+        int age;  
+        //creating two arg constructor  
+        Constex(int i,String n)
+        {  
+        id = i;  
+        name = n;  
+        }  
+        //creating three arg constructor  
+        Constex(int i,String n,int a){  
+        id = i;  
+        name = n;  
+        age=a;  
+        }  
+        void display(){System.out.println(id+" "+name+" "+age);}  
+       
+        public static void main(String args[]){  
+        Constex s1 = new Constex(111,"Karan");  
+        Constex s2 = new Constex(222,"Aryan",25);  
+        s1.display();  
+        s2.display();  
+       }  
+    }  
+    
