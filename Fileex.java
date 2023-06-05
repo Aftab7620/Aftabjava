a59ebf51650163344113292bc07710e513aaba65
Aftab7620 <mohdaftab7620@gmail.com>
2023-06-05 22:28:40 +0530

    Create Fileex.java

---
+++ Fileex.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Fileex
+{
+	public static void main(String[] args) throws IOException
+	{
+
+		
+		FileWriter fr=new FileWriter("E:\\Aftab.txt");
+		System.out.println("File Created!");
+	}
+}
